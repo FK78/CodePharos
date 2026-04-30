a3ffdc9059b2280ad06d83610133a15848d185e1
FK78 <34196381+FK78@users.noreply.github.com>
2026-04-30 13:22:06 +0100

    feat: issue JWTs on user login

--- src/server/controllers/usersController.js
+++ src/server/controllers/usersController.js
@@ -2,17 +2,17 @@ import * as usersQueries from "../queries/usersQueries.js";
 import * as usersService from "../services/usersService.js";
 
 export async function getByEmail(req, res) {
-  console.log(req.body)
   const user = await usersQueries.getByEmail(req.body.email);
   const hashObj = await usersService.hashPassword(
     req.body.password,
     Buffer.from(user.password_salt, "base64"),
   );
-  const userVerify = usersService.compareHash(
-    user.password_hash,
-    hashObj.hash,
-  );
-  userVerify ? res.status(200).json(userVerify) : res.status(401).send(userVerify);
+  const userVerify = usersService.compareHash(user.password_hash, hashObj.hash);
+  userVerify
+    ? res
+        .status(200)
+        .json(usersService.issueJWT({ id: user.id, role: user.role }))
+    : res.status(401).send(userVerify);
 }
 
 export async function create(req, res) {
